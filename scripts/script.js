1d9b36e4137e492d953dce136284ab0bee87b8f9
Nestor <nestorepa132004@gmail.com>
2026-02-25 17:48:24 -0600

    Cambiamos la suma por una resta

--- scripts/script.js
+++ scripts/script.js
@@ -3,12 +3,12 @@ let num1 = 10;
 let num2 = 20;
 
 // Suma
-let suma = num1 + num2;
+let suma = num1 - num2;
 
 // Mostrar alerta
-alert("La suma es: " + suma);
+alert("La resta es: " + suma);
 
 // Imprimir en consola
 console.log("El valor de num1 es:", num1);
 console.log("El valor de num2 es:", num2);
-console.log("La suma es:", suma);
+console.log("La resta es:", suma);
