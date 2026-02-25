066ba205399ca8e7a4a48d41971f0e99ee4ec94b
ManuelLPZ17 <alfonso.lopez@iteso.mx>
2026-02-25 17:29:58 -0600

    scripts agregado

---
+++ scripts/script.js
@@ -0,0 +1,14 @@
+// Declaración de variables
+let num1 = 10;
+let num2 = 20;
+
+// Suma
+let suma = num1 + num2;
+
+// Mostrar alerta
+alert("La suma es: " + suma);
+
+// Imprimir en consola
+console.log("El valor de num1 es:", num1);
+console.log("El valor de num2 es:", num2);
+console.log("La suma es:", suma);
